2f2f043781d4d38306b5ffcfd8c08a4018258f44
Marion Ochem <marion.ochem@gmail.com>
2024-03-19 15:23:19 +0100

    Resolved the issues with the for loops and structured the data output by the script

--- Scraping/bleauinfo.py
+++ Scraping/bleauinfo.py
@@ -4,6 +4,11 @@ from collections import Counter
 
 bleauinfo_url = "https://bleau.info/areas_by_region"
 
+# Final data : 
+all_routes_types = set()
+sites_name = []
+all_data = {}
+
 # Function to get all sites link 
 def scrape_sectors(url):
     sites_links = []
@@ -11,13 +16,14 @@ def scrape_sectors(url):
     response = requests.get(url)
     if response.status_code == 200:
             # Parse the html content of the page
-            soup = BeautifulSoup(response.content, "html.parser")
-            soup_result = soup.prettify().encode('ISO-8859-1')
+            soup = BeautifulSoup(response.content, "html.parser", from_encoding="utf-8") # ISO-8859-1
+            # soup_result = soup.prettify().encode('ISO-8859-1')
             # print(soup_result)
             divs = soup.find_all("div", class_=False)
             for div in divs:
-                 # Find all direct child <a> elements of each div
+                 # Find all direct/first child <a> elements of each div
                  sites_links.extend(div.find_all('a', recursive=False))
+               #   print(sites_links)
                  
             create_sites_url(sites_links)
             
@@ -25,7 +31,7 @@ def scrape_sectors(url):
             print('Failed to fetch bleau.info webstite :', response.status_code)
             return None
 
-# Function to get all sites data     
+# Function to construct url link of each sites     
 def create_sites_url(links):
       sites_url = []
       # Retrieve all the href 
@@ -35,29 +41,48 @@ def create_sites_url(links):
               site_url = "https://bleau.info" + href
               sites_url.append(site_url)
      #  print(sites_url)
+     # Iterate over each url link and scrap it 
       for sites in sites_url:
-           scrape_sites(sites)
+           scrape_site(sites)
+      print("ALL ROUTE TYPES :", all_routes_types)
+      print("FINAL ALL DATA : ", all_data)
 
-def scrape_sites(url):
-    all_types = []
+
+# Function to scrape each sites and retrieve its types
+def scrape_site(url):
+    routes_types = []
     response = requests.get(url)
     if response.status_code == 200:
          soup = BeautifulSoup(response.content, "html.parser", from_encoding="ISO-8859-1")
+         site = soup.h3 # Find the first h3 element
+         name = site.text.strip() # Extract the inner text of this h3 element 
+         sites_name.append(name) # Store the site name inside global array for later
          types = soup.find_all("span", class_="btype")
-     #     print(types)
          for t in types:
-              all_types.append(t.text)
-     #     print(all_types)
-         top_types(all_types)
+              routes_types.extend(t.text.split(', '))
+          #     print(routes_types)
+
+         # Adding the array of all the routes types of a site to a global array for later 
+         all_routes_types.update(routes_types)
+
+         # Getting each type and its count for a single site 
+         counted_routes_types = count_types(routes_types)
+
+         # Add the site name and its corresponding route type and counts to the dictionary
+         all_data[name] = counted_routes_types
+         print(all_data)
+
+
     else:
         print('Failed to fetch bleau.info webstite :', response.status_code)
         return None 
 
-# Function to get the top 3 of route type in the site
-def top_types(array):
+# Function to get the count of each route type in a site
+def count_types(array):
      type_counts = Counter(array)
-     top_type = type_counts.most_common(3)
-     print(top_type)
+     top_type = type_counts.most_common()
+     # print(top_type)
+     return top_type
 
 
 # scrape_sites('https://bleau.info/cuvier')
@@ -65,5 +90,5 @@ scrape_sectors(bleauinfo_url)
 
 # Where am i : 
 # I have a script that scrap over all sites references in bleau.info and 
-# returns the top 3 type of routes for each site. 
+# returns a dictionary where keys are sites names and values are each routes types you can find there and its counted occurrence. 
 
