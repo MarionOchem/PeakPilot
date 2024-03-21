dc3a43ed4646b58a063460ea92d6049d36f2cbf1
Marion Ochem <marion.ochem@gmail.com>
2024-03-21 19:36:59 +0100

    db tables creations and insertions + append it to the main

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from sys import path
 
 path.append('..\\Scraping')
+path.append('..\\DbInterface')
 
 from Scraping.bleauinfo import *
+from DbInterface.seed import * 
 
 bleauinfo_url = "https://bleau.info/areas_by_region"
 
 def main():
-    # Run scraping 
-    # Run db connexion and injection of scrapped data
-    
+    # Run scraping module
     all_sites_links = scrape_sectors(bleauinfo_url)
-    all_sites_url = create_sites_url(all_sites_links)
-    result = scrape_sites(all_sites_url)
+    all_sites_url = create_sites_url(all_sites_links) 
+    result = scrape_sites(all_sites_url) # return all_routes_types, sites_name, all_data
     print(result)
 
+    # Run the insertion into db module
+    executeInsertions(result[1], result[0], result[3])
  
-
-
-
 
 
 
