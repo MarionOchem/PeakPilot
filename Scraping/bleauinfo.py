b63df39724f6386c33cce4e2bad4f926472472a4
Marion Ochem <marion.ochem@gmail.com>
2024-03-22 16:23:07 +0100

    handle encoding error and add logger.info in the script

--- Scraping/bleauinfo.py
+++ Scraping/bleauinfo.py
@@ -1,13 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 from collections import Counter
-# TODO: logging : ad logs in the scraping 
+from unidecode import unidecode
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 # TODO: try except catch instead of if/else
-# TODO: handle encoding exception for character "É"
+# TODO: handle encoding exception for character "É" and others
+# TODO: fichier test : bleauinfo_test.py import bleauinfo.py function and test it 
 
 
 # Function to get all sites link 
 def scrape_sectors(url):
+    logger.info('Starting scrape_sectors -- scraping href')
     sites_links = []
     # Send the HTTP GET request to bleau.info
     response = requests.get(url)
@@ -22,11 +29,12 @@ def scrape_sectors(url):
             return sites_links
             
     else:
-            print('Failed to fetch bleau.info webstite :', response.status_code)
+            logger.error('Failed to fetch bleau.info webstite :', response.status_code)
             return None
 
 # Function to construct url link of each sites     
 def create_sites_url(links):
+      logger.info('Starting create_sites_url -- creating urls')
       sites_url = []
       # Retrieve all the href 
       for link in links:
@@ -40,6 +48,7 @@ def create_sites_url(links):
 
 # Function to scrape each sites and retrieve its types
 def scrape_sites(urls):
+    logger.info('Starting scrape_sites -- main scrap')
     all_routes_types = set()
     sites_name = []
     all_data = {}
@@ -49,16 +58,17 @@ def scrape_sites(urls):
         response = requests.get(url)
         
         if response.status_code == 200:
-            soup = BeautifulSoup(response.content, "html.parser", from_encoding="ISO-8859-1")
+            soup = BeautifulSoup(response.content, "html.parser", from_encoding="utf-8")
             site = soup.h3  # Find the first h3 element
             name = site.text.strip()  # Extract the inner text of this h3 element
-            name = name.replace("\x89", "E") # Handle a encoding exception 
-            sites_name.append(name)  # Store the site name inside global array for later
+            encodedName = unidecode(name) # encode string to avoid encoding exception
+            sites_name.append(encodedName)  # Store the site name inside global array for later
             
             types = soup.find_all("span", class_="btype")
             for t in types:
-                routes_types.extend(t.text.split(', '))
-                # print(routes_types)
+                split_types = t.text.split(', ')
+                for st in split_types:
+                    routes_types.append(unidecode(st))  # encode string to avoid encoding exception
 
             # Adding the array of all the routes types of a site to a global array for later 
             all_routes_types.update(routes_types)
@@ -67,10 +77,10 @@ def scrape_sites(urls):
             counted_routes_types = count_types(routes_types)
 
             # Add the site name and its corresponding route type and counts to the dictionary
-            all_data[name] = counted_routes_types
+            all_data[encodedName] = counted_routes_types
 
         else:
-            print('Failed to fetch bleau.info webstite :', response.status_code)
+            logger.error('Failed to fetch bleau.info webstite :', response.status_code)
             return None 
         
     return all_routes_types, sites_name, all_data
