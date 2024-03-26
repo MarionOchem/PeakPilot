4b359cd181de3aa2e00950e00ddb679b3b745ed7
Marion Ochem <marion.ochem@gmail.com>
2024-03-26 15:10:14 +0100

    restructure the data return as JSON in main endpoint

--- API/controllers/GetAll.go
+++ API/controllers/GetAll.go
@@ -7,31 +7,71 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Struct definition to represent the data of all sites_routes table data
-type SitesRoutes struct {
+type DbStructure struct {
 	SiteName string
 	RouteType string
-	RouteCount int 
+	RouteCount int
 }
 
+// Restructuring data before returning it as JSON :
+// Gorm maps the columns from the query result to the fields of those structs
+
+// Struct to represent the nested route data
+type NestedRoute struct {
+	RouteType  string
+	RouteCount int    
+}
+
+// Struct to represent the nested site data
+type NestedSite struct {
+    SiteName string
+    Routes []NestedRoute
+}
+
+// Determine if NestedSite already exists for the current site. If it does, append the current route tot he existing NestedSite, 
+// if it doesn't, create a new NestedSite for the current site. 
+func findSiteIndex(nestedRoutes []NestedSite, siteName string) int {
+    for i, nestedRoute := range nestedRoutes {
+        if nestedRoute.SiteName == siteName {
+            return i // Return the index of the site if found
+        }
+    }
+    return -1 // Return -1 if the site is not found
+}
+
+
 func GetAll(c *gin.Context) {
-	// Get all
-	var sitesRoutes []SitesRoutes
-	result := initializers.DB.Table("sites_routes").
-	Select("sites_routes.id AS site_route_id, sites.name AS site_name, routes.name AS route_type, sites_routes.route_count").
-	Joins("JOIN sites ON sites_routes.site_name = sites.id").
-	Joins("JOIN routes ON sites_routes.route_type = routes.id").
-	Find(&sitesRoutes)
-	
+	// Get all sites names, routes and routes counts data
+	var allNestedRoutes []NestedSite 
+
+		var siteRouteContent []DbStructure
+	result := initializers.DB.Table("site_route_content" ).Select("site_name", "route_type", "route_count").Find(&siteRouteContent)
+
 	if result.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve sites"})
 		return
 	}
 
-	// Return the list of sites in the JSON response
+	for _, route := range siteRouteContent {
+		nestedRoute := NestedRoute{
+			RouteType: route.RouteType,
+			RouteCount: route.RouteCount,
+		}
+		siteIndex := findSiteIndex(allNestedRoutes, route.SiteName)
+		if siteIndex == -1 {
+			allNestedRoutes = append(allNestedRoutes, NestedSite{
+				SiteName: route.SiteName,
+				Routes: []NestedRoute{nestedRoute},
+			})
+		} else {
+			allNestedRoutes[siteIndex].Routes = append(allNestedRoutes[siteIndex].Routes, nestedRoute)
+		}
+	}
+
+	// Return the list of sites names, routes and routes counts data in the JSON response
 	c.JSON(http.StatusOK, gin.H{
-		"message": "All routes of all sites",
-		"sites":   sitesRoutes,
+		"message": "All routes and their counts of all sites",
+		"data": allNestedRoutes,
 	})
 }
 
